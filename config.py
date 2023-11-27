fdef54d53d51881aa6d626872440216acf978067
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-11-27 22:11:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "21288218")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6934047363:AAECazs2XdRXWuQjL-sRyr0rvb5GFCTx20k") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '-1001956303989') # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", ") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', ') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://SNOWENCODER:SNOWENCODER@cluster0.hdxpxtg.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","SnowEncodingBot") 
+    DB_URL  = os.environ.get("DB_URL","")  # ⚠️ Required
+    DB_NAME  = os.environ.get("DB_NAME","") 
 
     # Other Configs 
-    ADMIN = os.environ.get("ADMIN", '6065594762') # ⚠️ Required
-    LOG_CHANNEL = os.environ.get('LOG_CHANNEL', '-1001971176803') # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", ')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
 
