2f0928120b217f2638a0c61811b8442a60ddc756
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-11-27 22:26:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ id_pattern = re.compile(r'^.\d+$')
 class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", ") # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', ') # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
@@ -17,7 +17,7 @@ class Config(object):
     DB_NAME  = os.environ.get("DB_NAME","") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", ')) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", '')) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
